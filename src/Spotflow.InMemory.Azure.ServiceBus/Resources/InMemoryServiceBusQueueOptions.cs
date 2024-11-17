edd6c88dc3c30331b185ad7a8af0fa1edcb33bc2
Tomas Pajurek <tomas.pajurek@spotflow.io>
2024-11-17 18:18:42 +0100

    Use classes with init-only props instead of records for Service Bus entities options

--- src/Spotflow.InMemory.Azure.ServiceBus/Resources/InMemoryServiceBusQueueOptions.cs
+++ src/Spotflow.InMemory.Azure.ServiceBus/Resources/InMemoryServiceBusQueueOptions.cs
@@ -1,3 +1,7 @@
 namespace Spotflow.InMemory.Azure.ServiceBus.Resources;
 
-public record InMemoryServiceBusQueueOptions(bool EnableSessions = false, TimeSpan? LockTime = null);
+public class InMemoryServiceBusQueueOptions
+{
+    public bool EnableSessions { get; init; } = false;
+    public TimeSpan? LockTime { get; init; } = null;
+}
