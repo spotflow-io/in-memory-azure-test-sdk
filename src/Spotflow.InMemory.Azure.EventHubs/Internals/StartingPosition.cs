4e4e6081d860e1dc7f890a86f94f6d6b61eaba2f
Vitalii Tokarchyn <tokarchyn@gmail.com>
2025-07-10 16:42:05 +0200

    Resolve the problem in backward compatible way

--- src/Spotflow.InMemory.Azure.EventHubs/Internals/StartingPosition.cs
+++ src/Spotflow.InMemory.Azure.EventHubs/Internals/StartingPosition.cs
@@ -45,9 +45,14 @@ internal readonly record struct InMemoryEventPosition(long SequenceNumber, bool 
 
         var isInclusive = ReflectionUtils.ReadInternalValueProperty<bool>(position, "IsInclusive");
 
-        var offset = ReflectionUtils.ReadInternalReferenceProperty<object>(position, "Offset");
+        if (ReflectionUtils.TryReadOptionalInternalReferenceProperty<object>(position, "OffsetString", out var offsetString) &&
+            offsetString is not null)
+        {
+            throw new NotSupportedException("EventPosition with offset is not supported.");
+        }
 
-        if (offset is not null)
+        if (ReflectionUtils.TryReadOptionalInternalReferenceProperty<object>(position, "Offset", out var offset) &&
+            offset is not null)
         {
             throw new NotSupportedException("EventPosition with offset is not supported.");
         }
