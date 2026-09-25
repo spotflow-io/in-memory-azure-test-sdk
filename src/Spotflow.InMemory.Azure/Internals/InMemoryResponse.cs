b21b4645a6aed434f72910d1178b851eac24b4d7
Tomas Pajurek <tomas.pajurek@spotflow.io>
2026-09-25 13:42:16 +0200

    Container lease & delete

--- src/Spotflow.InMemory.Azure/Internals/InMemoryResponse.cs
+++ src/Spotflow.InMemory.Azure/Internals/InMemoryResponse.cs
@@ -10,11 +10,13 @@ public class InMemoryResponse : Response
 {
     private readonly Dictionary<string, List<string>> _headers = [];
 
-    public static Response<T> FromValue<T>(T value, int status, ETag? eTag = null, IReadOnlyDictionary<string, string>? headers = null)
+    public static Response<T> FromValue<T>(T value, int status, ETag? eTag = null)
+        => FromValue(value, new InMemoryResponse(status, eTag));
+
+    public static Response<T> FromValue<T>(T value, int status, ETag? eTag, IReadOnlyDictionary<string, string> headers)
         => FromValue(value, new InMemoryResponse(status, eTag, headers));
 
-
-    public InMemoryResponse(int status, ETag? eTag = null, IReadOnlyDictionary<string, string>? headers = null)
+    public InMemoryResponse(int status, ETag? eTag, IReadOnlyDictionary<string, string>? headers)
     {
         Status = status;
         ClientRequestId = Guid.NewGuid().ToString();
@@ -31,7 +33,11 @@ public class InMemoryResponse : Response
                 AddHeaderValue(header.Key, header.Value);
             }
         }
+    }
 
+    public InMemoryResponse(int status, ETag? eTag = null) : this(status, eTag, null)
+    {
+        // Intentionally-empty        
     }
 
     public override int Status { get; }
